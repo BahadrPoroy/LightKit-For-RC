e748fb87f9d2aaaba6ff09b31ac01bca50da74a6
BahadrPoroy <56046468+BahadrPoroy@users.noreply.github.com>
2024-04-29 12:38:15 +0300

    Light-Kit Update & Fix
    New Features
    - Added welcome lighting
    - Added lighting on/off animation
    fixes
         Arduino was ignoring the signal so, these has been changed:
    - Input threshold was 1700, it's reduced to 1600
    - There were 50 milliseconds delay in main() it's been removed

--- Light-Kit/Light-Kit.ino
+++ Light-Kit/Light-Kit.ino
@@ -10,12 +10,12 @@
 //End of pin definitions
 
 //Variable settings
-int input, previousMillis = 0, difrence, nowMillis = 0; //readed value storing in 'input'
-bool state = 0; //It's used for control the lights (on/off)
+int input, previousMillis = 0, difrence, nowMillis = 0, i = 0;  //readed value storing in 'input'
+bool state = 0;                                                 //It's used for control the lights (on/off)
 //End of variable settings
 
 void setup() {
-  Serial.begin(9600); //Serial communication starts
+  Serial.begin(9600);  //Serial communication starts
   //Pin mode settings
   pinMode(inputPin, INPUT);
   pinMode(ledPin1, OUTPUT);
@@ -24,23 +24,51 @@ void setup() {
   pinMode(ledPin4, OUTPUT);
   pinMode(ledPin5, OUTPUT);
   //End of pin mode settings
+  for (i = 0; i < 3; i++) {
+    welcome_signal();
+  }
 }
 
 void loop() {
-  if (input > 1700) {   //if input value bigger than 1700 (button pressed), this will be activated.
-    nowMillis = millis(); //this line and next two lines are preventing unwanted blinkings
-    if((nowMillis - previousMillis) >= 150){  //If previous turn on/off command newer than 150 milliseconds program ignores new command
-      previousMillis = nowMillis;
-      state = !state; //LED states updating
-      //Updated state writing the LEDs
-      digitalWrite(ledPin1, state);
-      digitalWrite(ledPin2, state);
-      digitalWrite(ledPin3, state);
-      digitalWrite(ledPin4, state);
-      digitalWrite(ledPin5, state);
-      //End of writing
+  if (input > 1600) {                           //if input value bigger than 1700 (button pressed), this will be activated.
+    nowMillis = millis();                       //this line and next two lines are preventing unwanted blinkings
+    if ((nowMillis - previousMillis) >= 150) {  //If previous turn on/off command newer than 150 milliseconds program ignores new command
+      headlight();
     }
   }
-  input = pulseIn(inputPin, HIGH, 25000); // signal receiving is processing here. I have taken this and 45. lines from Beaver's Hobby Youtube Channel: Silly Arduino Project #1
-  delay(50);
+  input = pulseIn(inputPin, HIGH, 25000);  // signal receiving is processing here. I have taken this line from Beaver's Hobby Youtube Channel: Silly Arduino Project #1
+}
+
+void headlight() {
+  previousMillis = nowMillis;
+  state = !state;  //LED states updating
+  //Updated state writing the LEDs
+  digitalWrite(ledPin3, state);
+  delay(100);
+  digitalWrite(ledPin2, state);
+  digitalWrite(ledPin4, state);
+  delay(100);
+  digitalWrite(ledPin1, state);
+  digitalWrite(ledPin5, state);
+  delay(100);
+  //End of writing
+}
+
+void welcome_signal() {
+  digitalWrite(ledPin3, HIGH);
+  delay(100);
+  digitalWrite(ledPin4, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  delay(100);
+  digitalWrite(ledPin3, LOW);
+  delay(100);
+  digitalWrite(ledPin5, HIGH);
+  digitalWrite(ledPin1, HIGH);
+  delay(100);
+  digitalWrite(ledPin4, LOW);
+  digitalWrite(ledPin2, LOW);
+  delay(100);
+  digitalWrite(ledPin5, LOW);
+  digitalWrite(ledPin1, LOW);
+  delay(200);
 }
