91287e94a9eb8cb05c3e47dfdf0741700182800f
BahadrPoroy <Bahadr.poroy@gmail.com>
2024-11-10 17:37:05 +0300

    Update V1.1
    What's news
    + I've edited LED numbers (for my car)
    + Input threshold limit updated (from 1600 to 1850)
    + state variable has been removed
    + Most of delays have been removed
    + Light mode changing method has been changed
    + High beam and low beam functions have been added

--- Light-Kit/Light-Kit.ino
+++ Light-Kit/Light-Kit.ino
@@ -1,17 +1,12 @@
-// I have taken the signal reading method from here: Beaver's Hobby Youtube Channel: Silly Arduino Project #1
-
 //Pin definitions
 #define inputPin 3
 #define ledPin1 4
 #define ledPin2 5
 #define ledPin3 6
-#define ledPin4 7
-#define ledPin5 8
 //End of pin definitions
 
 //Variable settings
-int input, previousMillis = 0, difrence, nowMillis = 0, i = 0;  //readed value storing in 'input'
-bool state = 0;                                                 //It's used for control the lights (on/off)
+int input, previousMillis = 0, nowMillis = 0, i = 0, mode = 0;  //readed value storing in 'input', 'i' used for welcoming lights, 'mode' used for controlling the light modes
 //End of variable settings
 
 void setup() {
@@ -21,54 +16,78 @@ void setup() {
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
-  pinMode(ledPin5, OUTPUT);
   //End of pin mode settings
-  for (i = 0; i < 3; i++) {
-    welcome_signal();
+  for (i = 0; i <= 3; i++) {
+    welcoming();
   }
 }
 
 void loop() {
-  if (input > 1600) {                           //if input value bigger than 1700 (button pressed), this will be activated.
-    nowMillis = millis();                       //this line and next two lines are preventing unwanted blinkings
-    if ((nowMillis - previousMillis) >= 150) {  //If previous turn on/off command newer than 150 milliseconds program ignores new command
-      headlight();
-    }
+
+  switch (mode) {
+    case (0):
+      noLight();
+      break;
+    case (1):
+      lowBeam();
+      break;
+    case (2):
+      highBeam();
+      break;
   }
   input = pulseIn(inputPin, HIGH, 25000);  // signal receiving is processing here. I have taken this line from Beaver's Hobby Youtube Channel: Silly Arduino Project #1
+  previousMillis = millis();               //this line and next three lines are preventing unwanted blinkings
+  nowMillis = millis();
+  while (nowMillis - previousMillis <= 150) {
+    nowMillis = millis();
+  }
+  if (input >= 1850) {  //if input value bigger than 1850 (button pressed), this will be activated.
+    if (mode == 0) {
+      mode = (mode + 1) % 3;
+    } else if (mode == 1) {
+      mode = (mode + 1) % 3;
+    } else if (mode == 2) {
+      mode = (mode + 1) % 3;
+    }
+  } else {
+    mode = mode;
+  }
+  Serial.print("İnput: ");
+  Serial.println(input);
+  Serial.println(mode);
 }
 
-void headlight() {
-  previousMillis = nowMillis;
-  state = !state;  //LED states updating
-  //Updated state writing the LEDs
-  digitalWrite(ledPin3, state);
-  delay(100);
-  digitalWrite(ledPin2, state);
-  digitalWrite(ledPin4, state);
-  delay(100);
-  digitalWrite(ledPin1, state);
-  digitalWrite(ledPin5, state);
-  delay(100);
+void lowBeam() {
+  // writing new states of the LEDs
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin3, LOW);
   //End of writing
 }
 
-void welcome_signal() {
-  digitalWrite(ledPin3, HIGH);
-  delay(100);
-  digitalWrite(ledPin4, HIGH);
+void highBeam() {
+  // writing new states of the LEDs
+  digitalWrite(ledPin1, HIGH);
   digitalWrite(ledPin2, HIGH);
-  delay(100);
+  digitalWrite(ledPin3, HIGH);
+  //End of writing
+}
+
+void noLight() {
+  // writing new states of the LEDs
+  digitalWrite(ledPin3, LOW);
+  digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin1, LOW);
+  //End of writing
+}
+
+void welcoming() {
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, HIGH);
   digitalWrite(ledPin3, LOW);
   delay(100);
-  digitalWrite(ledPin5, HIGH);
   digitalWrite(ledPin1, HIGH);
-  delay(100);
-  digitalWrite(ledPin4, LOW);
   digitalWrite(ledPin2, LOW);
-  delay(100);
-  digitalWrite(ledPin5, LOW);
-  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin3, HIGH);
   delay(200);
 }
